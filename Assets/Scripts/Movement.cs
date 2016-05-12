bdd7f5e043965d44ca633d701f4fd9acc395b04b
Krishna <4953193@student.swin.edu.au>
2016-05-12 21:21:52 +1000

    as

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float paddleSpeed = 1f;
+
+    private Vector3 playerPos = new Vector3(0, -9.5f, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        playerPos = new Vector3(Mathf.Clamp(xPos, -8f, +8f), -9.5f, 0f);
+        transform.position = playerPos;
+    }
+}
